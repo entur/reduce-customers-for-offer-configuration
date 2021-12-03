0ae1ec552533d9757a5a1f991d74980c93c997ba
Kent Andersen <kent.andersen@entur.org>
2021-12-03 10:30:29 +0100

    :white_check_mark: Added tests for OfferSummery from Offers 2.0

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,6 +3,8 @@ import {
   reduceCustomersForOfferConfiguration,
   removeAllButTheFirstOccurrenceOfEachEntitlementFromCustomers
 } from '../source';
+import offer2ForFlexibleTicketWhichRequiresEntitlements from './data/offer2ForFlexibleTicketWhichRequiresEntitlements.json';
+import offer2ForFlexibleTicketWhichRequiresNoEntitlements from './data/offer2ForFlexibleTicketWhichRequiresNoEntitlements.json';
 import offerForFlexibleTicketWhichRequiresEntitlements from './data/offerForFlexibleTicketWhichRequiresEntitlements.json';
 import offerForFlexibleTicketWhichRequiresNoEntitlements from './data/offerForFlexibleTicketWhichRequiresNoEntitlements.json';
 import offerForSeatReservationSeasonTicket from './data/offerForSeatReservationSeasonTicket.json';
@@ -14,6 +16,17 @@ describe('reduceCustomersForOfferConfiguration', () => {
         [],
         {offerId: 'aa004b4e-c539-4fd8-bc1f'},
         offerForFlexibleTicketWhichRequiresEntitlements
+      )
+    ).toThrow(
+      'offer.id and offerConfiguration.offerId do not match; they must be the same'
+    );
+  });
+  test('Offer 2.0 Throws error if offerConfiguration.offerId is not the same as offer.id', () => {
+    expect(() =>
+      reduceCustomersForOfferConfiguration(
+        [],
+        {offerId: 'aa004b4e-c539-4fd8-bc1f'},
+        offer2ForFlexibleTicketWhichRequiresEntitlements
       )
     ).toThrow(
       'offer.id and offerConfiguration.offerId do not match; they must be the same'
@@ -31,11 +44,29 @@ describe('extractIdsOfEntitlementProductsRequiredToPurchaseOffer', () => {
     ).toEqual(new Set());
   });
 
+  test('Offer 2.0 Returns no IDs when no entitlements are required', () => {
+    expect(
+      extractIdsOfEntitlementProductsRequiredToPurchaseOffer(
+        [],
+        offer2ForFlexibleTicketWhichRequiresNoEntitlements
+      )
+    ).toEqual(new Set());
+  });
+
   test('Returns the IDs required by mandatory products when no selectableProductIds are supplied', () => {
     expect(
       extractIdsOfEntitlementProductsRequiredToPurchaseOffer(
         [],
         offerForFlexibleTicketWhichRequiresEntitlements
+      )
+    ).toEqual(new Set(['ENT:EntitlementProduct:levelA2']));
+  });
+
+  test('Offer 2.0 Returns the IDs required by mandatory products when no selectableProductIds are supplied', () => {
+    expect(
+      extractIdsOfEntitlementProductsRequiredToPurchaseOffer(
+        [],
+        offer2ForFlexibleTicketWhichRequiresEntitlements
       )
     ).toEqual(new Set(['ENT:EntitlementProduct:levelA2']));
   });
@@ -54,6 +85,15 @@ describe('extractIdsOfEntitlementProductsRequiredToPurchaseOffer', () => {
       extractIdsOfEntitlementProductsRequiredToPurchaseOffer(
         ['IZnkFy'],
         offerForFlexibleTicketWhichRequiresEntitlements
+      )
+    ).toEqual(new Set(['ENT:EntitlementProduct:levelA2']));
+  });
+
+  test('Offer 2.0 Returns only the IDs required by mandatory products when only selectableProductIds for products that do not require entitlements are supplied', () => {
+    expect(
+      extractIdsOfEntitlementProductsRequiredToPurchaseOffer(
+        ['6nbdNy'],
+        offer2ForFlexibleTicketWhichRequiresEntitlements
       )
     ).toEqual(new Set(['ENT:EntitlementProduct:levelA2']));
   });
