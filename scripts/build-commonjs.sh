fbde18823c4bf3f8ce44a58cded4ea4b7e141efd
Frank Sondre Waldal <frank.sondre.waldal@entur.org>
2022-06-03 14:57:36 +0200

    make build scripts os independent, add script for esm build, update package.json

--- scripts/build-commonjs.sh
+++ scripts/build-commonjs.sh
@@ -2,7 +2,14 @@
 
 npx tsc --module commonjs --target es5 --outDir dist/commonjs
 
+SED_OPTIONS=
+    if [[ "$OSTYPE" == "darwin" ]]; then
+        SED_OPTIONS="-E -i ''"
+    else
+        SED_OPTIONS="-E -i"
+    fi
+
 for f in dist/commonjs/*.js; do
-    sed -E -i '' 's/require\("(\.\/.*)"\)/require\("\1\.cjs"\)/g' $f
+    sed $SED_OPTIONS 's/require\("(\.\/.*)"\)/require\("\1\.cjs"\)/g' $f
     mv "$f" "${f%.js}.cjs"
 done
