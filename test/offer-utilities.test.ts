cc7c8326bd03ef5ceed3bed7334c7100a7c607b5
Eirik Vågeskar <playground.gate@protonmail.com>
2023-05-11 13:08:02 +0200

    Use Vite for building the app

--- test/offer-utilities.test.ts
+++ test/offer-utilities.test.ts
@@ -1,11 +1,11 @@
-import {expect} from 'expect';
+import {describe, expect, it} from 'vitest';
 
 import {extractIdsOfEntitlementProductsRequiredToPurchaseOffer} from '../source/offer-utilities.js';
 import offerForFlexibleTicketWhichRequiresEntitlements from './data/offerForFlexibleTicketWhichRequiresEntitlements.json' assert {type: 'json'};
 import offerForFlexibleTicketWhichRequiresNoEntitlements from './data/offerForFlexibleTicketWhichRequiresNoEntitlements.json' assert {type: 'json'};
 
 describe('extractIdsOfEntitlementProductsRequiredToPurchaseOffer', () => {
-  describe('Returns no IDs when no entitlements are required', () => {
+  it('Returns no IDs when no entitlements are required', () => {
     expect(
       extractIdsOfEntitlementProductsRequiredToPurchaseOffer(
         [],
@@ -14,7 +14,7 @@ describe('extractIdsOfEntitlementProductsRequiredToPurchaseOffer', () => {
     ).toEqual(new Set());
   });
 
-  describe('Returns the IDs required by mandatory products when no selectableProductIds are supplied', () => {
+  it('Returns the IDs required by mandatory products when no selectableProductIds are supplied', () => {
     expect(
       extractIdsOfEntitlementProductsRequiredToPurchaseOffer(
         [],
@@ -23,7 +23,7 @@ describe('extractIdsOfEntitlementProductsRequiredToPurchaseOffer', () => {
     ).toEqual(new Set(['ENT:EntitlementProduct:levelA2']));
   });
 
-  describe('Returns the IDs required by mandatory products and selectable products when selectableProductIds for products that require entitlements are supplied', () => {
+  it('Returns the IDs required by mandatory products and selectable products when selectableProductIds for products that require entitlements are supplied', () => {
     expect(
       extractIdsOfEntitlementProductsRequiredToPurchaseOffer(
         ['6nbdNy'],
