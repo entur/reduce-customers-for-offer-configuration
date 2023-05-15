ed92cf11fa918ded04a3cb474dcc7584ebd66677
Eirik Vågeskar <playground.gate@protonmail.com>
2023-05-15 10:51:28 +0200

    Delete unused script

--- scripts/build-commonjs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-npx tsc --module commonjs --target es5 --outDir dist/commonjs
-
-for f in dist/commonjs/*.js; do
-    sed -E 's/require\("(\.\/.*).js"\)/require\("\1\.cjs"\)/g' "$f" > "${f%.js}.cjs"
-    rm "$f"
-done
